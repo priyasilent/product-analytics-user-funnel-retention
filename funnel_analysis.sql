60a12862a7c55ea30878977fd43108890b1d8fe8
Debopriya Das <dasdebo16@gmail.com>
2026-03-20 09:35:44 +0100

    Update funnel_analysis.sql

--- funnel_analysis.sql
+++ funnel_analysis.sql
@@ -1,6 +1,10 @@
 SELECT
     onboarding_step,
-    COUNT(DISTINCT user_id) AS users_at_step
+    COUNT(DISTINCT user_id) AS users,
+    ROUND(
+        COUNT(DISTINCT user_id) * 100.0 / 
+        SUM(COUNT(DISTINCT user_id)) OVER (), 
+    2) AS percentage_of_total
 FROM user_onboarding
 GROUP BY onboarding_step
 ORDER BY onboarding_step;
