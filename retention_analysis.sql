79212a183078ccdcdc5206817dbd7ecfa45b790c
Debopriya Das <dasdebo16@gmail.com>
2026-03-20 09:32:51 +0100

    Update retention_analysis.sql

--- retention_analysis.sql
+++ retention_analysis.sql
@@ -1,1 +1,12 @@
-
+SELECT
+    signup_date,
+    COUNT(DISTINCT user_id) AS total_users,
+    COUNT(DISTINCT CASE 
+        WHEN activity_date = signup_date + INTERVAL '1 day' 
+        THEN user_id END) AS day_1_retained,
+    COUNT(DISTINCT CASE 
+        WHEN activity_date = signup_date + INTERVAL '7 day' 
+        THEN user_id END) AS day_7_retained
+FROM user_activity
+GROUP BY signup_date
+ORDER BY signup_date;
